ff402639639c08af6cd0c8ef35b96b92a3d652ec
Taylor Roberts <taylorroberts333@pointloma.edu>
2023-02-20 17:08:36 -0800

    adding

---
+++ Assets/TutorialInfo/Scripts/GameControllerScript.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+    Tilescript[] tiles;
+
+    public Tilescript start, end;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiles = FindObjectsOfType<Tilescript>();
+        for(int i=0; i < tiles.Length; i++) 
+        {
+            for(int j= i+1; j<tiles.Length; j++)
+            {
+                if(Vector3.Distance(tiles[i].transform.position,tiles[j].transform.position) < 1.1f)
+                {
+                    tiles[i].getNeighbors().Add(tiles[j]);
+                    tiles[j].getNeighbors().Add(tiles[i]);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        computePath(start, end);
+    }
+
+    List<Tilescript> tilesQueue = new List<Tilescript>();
+
+    public void computePath(Tilescript start, Tilescript end)
+    {
+        //clear all nodes from past computing
+        for(int i=0;i<tiles.Length; i++)
+        {
+            tiles[i].clear();
+        }
+
+        Tilescript current;
+
+        //start of dijkstra
+        start.setDistance(0);
+        tilesQueue.Clear();
+        tilesQueue.Add(start);
+
+        while (tilesQueue.Count > 0)
+        {
+            //find least tile
+            int smallestIndex = 0;
+            for (int i = 1; i<tilesQueue.Count; i++)
+            {
+                if (tilesQueue[i].getDistance() < tilesQueue[smallestIndex].getDistance())
+                {
+                    smallestIndex = i;
+                }
+            }
+
+            current = tilesQueue[smallestIndex];
+            tilesQueue.RemoveAt(smallestIndex);
+
+            current.setVisited(true);
+
+            if(current == end)
+            {
+                break;
+            }
+
+            for(int i = 0; i < current.getNeighbors().Count; i++)
+            {
+                if(!current.getNeighbors()[i].getVisited())
+                {
+                    //not previously seen
+                    if(current.getNeighbors()[i].getDistance() == float.MaxValue)
+                    {
+                        tilesQueue.Add(current.getNeighbors()[i]);
+                    }
+
+                    //if you use different terrain, not 1, have to keep track of weights
+                    float discreteDistance = current.getDistance() + 1;
+
+                    if(current.getNeighbors()[i].getDistance() > discreteDistance)
+                    {
+                        current.getNeighbors()[i].setDistance(discreteDistance);
+                        current.getNeighbors()[i].setBackPointer(current);
+                    }
+
+                }
+            }
+        }
+
+        temp.Clear();
+        path.Clear();
+        current = end;
+        while(current != start && current != null)
+        {
+            //current is the path from each node starting from the back to the front
+            temp.Add(current);
+            current = current.getBackPointer();
+        }
+        temp.Add(start);
+
+        for(int i = temp.Count - 1; i>=0; i--)
+        {
+            temp[i].setColor(Color.green * 3);
+            path.Add(temp[i]);
+        }
+
+    }
+
+    public List<Tilescript> temp = new List<Tilescript>();
+    public List<Tilescript> path = new List<Tilescript>();
+}
---
+++ Assets/TutorialInfo/Scripts/Tilescript.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilescript : MonoBehaviour
+{
+    public List<Tilescript> neighbors = new List<Tilescript>();
+
+    //if keeping track of weights
+    //public List<int> weights = new List<int>();
+
+    public float distance;
+    public Tilescript backPointer;
+    public bool hasVisited;
+
+    public GameObject[] borders;
+
+    public List<Tilescript> getNeighbors()
+    {
+        return neighbors;
+    }
+
+    public void clear()
+    {
+        distance = float.MaxValue;
+        hasVisited = false;
+        backPointer = null;
+
+        borders[0].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
+        borders[1].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
+        borders[2].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
+        borders[3].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
+    }
+
+    public void setDistance(float distance_in)
+    {
+        distance = distance_in;
+    }
+
+    public float getDistance()
+    {
+        return distance;
+    }
+
+    public void setVisited(bool visited_in)
+    {
+        hasVisited = visited_in;
+    }
+
+    public bool getVisited()
+    {
+        return hasVisited;
+    }
+
+    public void setBackPointer(Tilescript backPlace)
+    {
+        backPointer = backPlace;
+    }
+
+    public Tilescript getBackPointer()
+    {
+        return backPointer;
+    }
+
+    public void setColor(Color theColor)
+    {
+        borders[0].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
+        borders[1].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
+        borders[2].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
+        borders[3].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
+    }
+}

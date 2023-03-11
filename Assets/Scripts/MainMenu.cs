fda75dc8b825b36992edefde52606420b63ecc2a
joeyCampbelll <joey.m.campbell@gmail.com>
2023-03-10 19:32:19 -0800

    basic game customization menu created

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -14,4 +14,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void backToMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
 }

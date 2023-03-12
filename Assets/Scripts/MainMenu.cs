16a3b58db6d877e971b1d4805d4ae8dabf51908b
joeyCampbelll <joey.m.campbell@gmail.com>
2023-03-11 16:50:45 -0800

    formatted scripts

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,63 +6,63 @@ using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    public TMP_InputField wizardInput;
-    public TMP_InputField clericInput;
-    public TMP_InputField enemyInput;
-    public TextMeshProUGUI errorWizardClericText;
-    public TextMeshProUGUI errorEnemyText;
+	public TMP_InputField wizardInput;
+	public TMP_InputField clericInput;
+	public TMP_InputField enemyInput;
+	public TextMeshProUGUI errorWizardClericText;
+	public TextMeshProUGUI errorEnemyText;
 
-    public int numWizards;
-    public int numClerics;
-    public int numSkeletons;
-    public int numWarSkeletons;
+	public int numWizards;
+	public int numClerics;
+	public int numSkeletons;
+	public int numWarSkeletons;
 
-    public void startGameButton()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+	public void startGameButton()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
 
-    public void quitGameButton()
-    {
-        Application.Quit();
-    }
+	public void quitGameButton()
+	{
+		Application.Quit();
+	}
 
-    public void backToMainMenuButton()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
+	public void backToMainMenuButton()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+	}
 
-    public void nextButton()
-    {
-        if (int.Parse(wizardInput.text) + int.Parse(clericInput.text) != 5)
-        {
-            errorWizardClericText.enabled = true;
-        }
-        else
-        {
-            errorWizardClericText.enabled = false;
-        }
-        
-        if (int.Parse(enemyInput.text) > 10 || int.Parse(enemyInput.text) < 1)
-        {
-            errorEnemyText.enabled = true;
-        }
-        else
-        {
-            errorEnemyText.enabled = false;
-        }
+	public void nextButton()
+	{
+		if (int.Parse(wizardInput.text) + int.Parse(clericInput.text) != 5)
+		{
+			errorWizardClericText.enabled = true;
+		}
+		else
+		{
+			errorWizardClericText.enabled = false;
+		}
 
-        if (int.Parse(enemyInput.text) <= 10 && (int.Parse(wizardInput.text) + int.Parse(clericInput.text) == 5))
-        {
-            this.numWizards = int.Parse(wizardInput.text);
-            this.numClerics = int.Parse(clericInput.text);
+		if (int.Parse(enemyInput.text) > 10 || int.Parse(enemyInput.text) < 1)
+		{
+			errorEnemyText.enabled = true;
+		}
+		else
+		{
+			errorEnemyText.enabled = false;
+		}
 
-            int numEnemies = int.Parse(enemyInput.text);
+		if (int.Parse(enemyInput.text) <= 10 && (int.Parse(wizardInput.text) + int.Parse(clericInput.text) == 5))
+		{
+			this.numWizards = int.Parse(wizardInput.text);
+			this.numClerics = int.Parse(clericInput.text);
 
-            this.numSkeletons = numEnemies / 2;
-            this.numWarSkeletons = numEnemies - numSkeletons;
+			int numEnemies = int.Parse(enemyInput.text);
 
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
+			this.numSkeletons = numEnemies / 2;
+			this.numWarSkeletons = numEnemies - numSkeletons;
+
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
 }
--- Assets/Scripts/Tilescript.cs
+++ Assets/Scripts/Tilescript.cs
@@ -4,69 +4,69 @@ using UnityEngine;
 
 public class Tilescript : MonoBehaviour
 {
-    public List<Tilescript> neighbors = new List<Tilescript>();
+	public List<Tilescript> neighbors = new List<Tilescript>();
 
-    //if keeping track of weights
-    //public List<int> weights = new List<int>();
+	//if keeping track of weights
+	//public List<int> weights = new List<int>();
 
-    public float distance;
-    public Tilescript backPointer;
-    public bool hasVisited;
+	public float distance;
+	public Tilescript backPointer;
+	public bool hasVisited;
 
-    public GameObject[] borders;
+	public GameObject[] borders;
 
-    public List<Tilescript> getNeighbors()
-    {
-        return neighbors;
-    }
+	public List<Tilescript> getNeighbors()
+	{
+		return neighbors;
+	}
 
-    public void clear()
-    {
-        distance = float.MaxValue;
-        hasVisited = false;
-        backPointer = null;
+	public void clear()
+	{
+		distance = float.MaxValue;
+		hasVisited = false;
+		backPointer = null;
 
-        borders[0].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
-        borders[1].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
-        borders[2].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
-        borders[3].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
-    }
+		borders[0].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
+		borders[1].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
+		borders[2].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
+		borders[3].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.black);
+	}
 
-    public void setDistance(float distance_in)
-    {
-        distance = distance_in;
-    }
+	public void setDistance(float distance_in)
+	{
+		distance = distance_in;
+	}
 
-    public float getDistance()
-    {
-        return distance;
-    }
+	public float getDistance()
+	{
+		return distance;
+	}
 
-    public void setVisited(bool visited_in)
-    {
-        hasVisited = visited_in;
-    }
+	public void setVisited(bool visited_in)
+	{
+		hasVisited = visited_in;
+	}
 
-    public bool getVisited()
-    {
-        return hasVisited;
-    }
+	public bool getVisited()
+	{
+		return hasVisited;
+	}
 
-    public void setBackPointer(Tilescript backPlace)
-    {
-        backPointer = backPlace;
-    }
+	public void setBackPointer(Tilescript backPlace)
+	{
+		backPointer = backPlace;
+	}
 
-    public Tilescript getBackPointer()
-    {
-        return backPointer;
-    }
+	public Tilescript getBackPointer()
+	{
+		return backPointer;
+	}
 
-    public void setColor(Color theColor)
-    {
-        borders[0].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
-        borders[1].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
-        borders[2].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
-        borders[3].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
-    }
+	public void setColor(Color theColor)
+	{
+		borders[0].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
+		borders[1].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
+		borders[2].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
+		borders[3].GetComponent<MeshRenderer>().material.SetColor("_BaseColor", theColor);
+	}
 }

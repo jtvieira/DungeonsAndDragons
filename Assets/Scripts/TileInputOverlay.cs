46fa969a47102690515a363f165f5359ee28ecd9
Taylor Roberts <taylorroberts333@pointloma.edu>
2023-03-15 20:26:30 -0700

    moveOverlay

---
+++ Assets/Scripts/TileInputOverlay.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileInputOverlay : MonoBehaviour
+{
+    [SerializeField] private RectTransform panelRectTransform;
+    [SerializeField] private Button closeButton;
+    [SerializeField] private InputField inputField;
+
+
+    private void Start()
+    {
+        // Add listener to the close button
+        closeButton.onClick.AddListener(CloseOverlay);
+        //HideMovePanel();
+        ShowMovePanel();
+    }
+
+    private void CloseOverlay()
+    {
+        // Destroy the overlay game object
+        Destroy(gameObject);
+    }
+
+    public void ShowMovePanel()
+    {
+        panelRectTransform.gameObject.SetActive(true);
+    }
+
+    // Set the RectTransform object as not visible
+    public void HideMovePanel()
+    {
+        panelRectTransform.gameObject.SetActive(false);
+    }
+
+    // Get the text from the input field
+    public string GetInputText()
+    {
+        return inputField.text;
+    }
+}

57468f74b7b855064ca0fb1be7e555ec3d1db5f1
joeyCampbelll <joey.m.campbell@gmail.com>
2023-03-16 16:20:23 -0700

    debugging input overlay - joey

--- Assets/Scripts/TileInputOverlay.cs
+++ Assets/Scripts/TileInputOverlay.cs
@@ -3,38 +3,37 @@ using UnityEngine.UI;
 
 public class TileInputOverlay : MonoBehaviour
 {
-    [SerializeField] private RectTransform panelRectTransform;
-    [SerializeField] private Button closeButton;
-    [SerializeField] private InputField inputField;
+	[SerializeField] private RectTransform panelRectTransform;
+	[SerializeField] private Button closeButton;
+	[SerializeField] private InputField inputField;
 
+	private void Start()
+	{
+		ShowMovePanel();
+		// Add listener to the close button
+		closeButton.onClick.AddListener(CloseOverlay);
+	}
 
-    private void Start()
-    {
-        ShowMovePanel();
-        // Add listener to the close button
-        closeButton.onClick.AddListener(CloseOverlay);
-    }
+	private void CloseOverlay()
+	{
+		// Destroy the overlay game object
+		Destroy(gameObject);
+	}
 
-    private void CloseOverlay()
-    {
-        // Destroy the overlay game object
-        Destroy(gameObject);
-    }
+	public void ShowMovePanel()
+	{
+		panelRectTransform.gameObject.SetActive(true);
+	}
 
-    public void ShowMovePanel()
-    {
-        panelRectTransform.gameObject.SetActive(true);
-    }
+	// Set the RectTransform object as not visible
+	public void HideMovePanel()
+	{
+		panelRectTransform.gameObject.SetActive(false);
+	}
 
-    // Set the RectTransform object as not visible
-    public void HideMovePanel()
-    {
-        panelRectTransform.gameObject.SetActive(false);
-    }
-
-    // Get the text from the input field
-    public string GetInputText()
-    {
-        return inputField.text;
-    }
+	// Get the text from the input field
+	public string GetInputText()
+	{
+		return inputField.text;
+	}
 }

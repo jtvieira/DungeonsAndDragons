ca1790fedb78da5c657a5b0ce189ee76548a1efe
joeyCampbelll <joey.m.campbell@gmail.com>
2023-03-17 21:57:09 -0700

    hid the overlays

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -11,8 +11,8 @@ public class GameControllerScript : MonoBehaviour
 	private List<string> moveOrder;
 
 	// UI Overlay objects
-	
-	private 
+	private MoveChoiceOverlay moveChoiceOverlay;
+	private TileInputOverlay tileInputOverlay;
 
 	// Start is called before the first frame update
 	void Start()
@@ -24,6 +24,15 @@ public class GameControllerScript : MonoBehaviour
 		this.tiles = initController.getTiles();			
 		this.characters = initController.getCharacters();
 		this.moveOrder = initController.getMoveOrder();
+
+		// Get the existing Overlay script components
+		// **NOTE - every overlay we add will have to be added here
+		this.moveChoiceOverlay = FindObjectOfType<MoveChoiceOverlay>();
+		this.tileInputOverlay = FindObjectOfType<TileInputOverlay>();
+
+		// Hide the overlays at the beginning of the program
+		this.moveChoiceOverlay.hideMoveChoiceOverlay();
+		this.tileInputOverlay.hideTileInputOverlay();
 
 		executeTurnSystem();
 	}
@@ -54,7 +63,7 @@ public class GameControllerScript : MonoBehaviour
 
 	private void executePlayerMove(Character characterPlayer)
 	{
-
+		
 	}
 
 	// AI code goes here (at some point)
